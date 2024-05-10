cd631fe43bb91f6f92ae3e4a53ff9c888a8bf584
s28222 <s28222@pjwstk.edu.pl>
2024-05-10 16:31:45 +0200

    Implement VideocardDto

---
+++ ABPD_Test_1/DTOs/VideocardCreateDto.cs
@@ -0,0 +1,8 @@
+namespace ABPD_Test_1.DTOs;
+
+public class VideocardCreateDto
+{
+    public string Name { get; set; }
+    public float Frequency { get; set; }
+    public float Memory { get; set; }
+}
